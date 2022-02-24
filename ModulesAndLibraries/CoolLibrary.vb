c2cf2a30b7dc4b46a42bf4ba89260db95a2da927
Jamison Burton <jamison@burtontalk.com>
2022-02-24 08:54:46 -0700

    Add project files.

---
+++ ModulesAndLibraries/CoolLibrary.vb
@@ -0,0 +1,26 @@
+﻿Module CoolLibrary
+    ''' <summary>
+    ''' A full featured function to say hello...
+    ''' </summary>
+    ''' <returns>A string "Hello World!"</returns>
+    Function SayHello() As String
+        Return "Hello World!"
+    End Function
+
+    ''' <summary>
+    ''' Displays the content of arguement displayMessage then returns the user response.
+    ''' </summary>
+    ''' <param name="displayMessage"></param>
+    ''' <returns>string: user response</returns>
+    Public Function Prompt(ByVal Optional displayMessage As String = "<blank>") As String
+        Console.WriteLine(displayMessage)
+        Return Console.ReadLine()
+    End Function
+
+    Public Function RandomWithInRange(max As Integer, min As Integer) As Integer
+        Randomize(DateTime.Now.Millisecond)
+        max = max - min
+        Return CInt(Rnd() * max)
+    End Function
+
+End Module
---
+++ ModulesAndLibraries/ModulesAndLibraries.vb
@@ -0,0 +1,22 @@
+﻿Imports CoolLibrary
+
+Module ModulesAndLibraries
+
+    Sub Main()
+        Dim temp As String
+        Dim menu As String
+
+        'menu = "1. Delete" & vbNewLine & "2. Save" & vbNewLine & "3. Rename" & vbNewLine
+
+        ''CoolLibrary.SayHello()
+        ''Console.WriteLine(CoolLibrary.SayHello)
+        'CoolLibrary.Prompt("Yo! Type something!")
+        'temp = CoolLibrary.Prompt(menu)
+
+
+        Prompt()
+
+        Console.Read()
+    End Sub
+
+End Module
